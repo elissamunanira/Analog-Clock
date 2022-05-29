3cbbd81cd639c3a80c58dadb9f805899c186b04f
ELISSA-MUNANIRA <89012918+elissamunanira@users.noreply.github.com>
2022-05-29 21:44:33 +0200

    Add files via upload

---
+++ analogclock.js
@@ -0,0 +1,20 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.getElementById("data-hour-hand")
+const minuteHand = document.getElementById("data-minute-hand")
+const secondHand = document.getElementById("data-second-hand")
+
+function setClock(){
+    const currentDate = new Date()
+     let secondsRatio = currentDate.getSeconds()/60
+     let minutesRatio =(secondsRatio + currentDate.getMinutes())/60 
+     let hoursRatio = (minutesRatio + currentDate.getHours())/12
+     setRotation(secondHand, secondsRatio)
+     setRotation(minuteHand, minutesRatio)
+     setRotation(hourHand, hoursRatio)
+}
+function setRotation(element,rotationRatio){
+element.style.setProperty('--rotation', rotationRatio * 360)
+}
+setClock(); 
+//ctrl+d so usefull to change word everywhere it is
